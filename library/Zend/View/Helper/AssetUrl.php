ec09a8351569bdf19d49ef28f53d09f4ebe2c139
Andreas Möller <a@softe.is>
2012-12-04 14:26:06 +0100

    Overhaul implementation of a simple asset url view helper

--- library/Zend/View/Helper/AssetUrl.php
+++ library/Zend/View/Helper/AssetUrl.php
@@ -1,77 +1,139 @@
 <?php
 /**
- * Zend Framework
+ * Zend Framework (http://framework.zend.com/)
  *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://framework.zend.com/license/new-bsd
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@zend.com so we can send you a copy immediately.
- *
- * @category   Zend
- * @package    Zend_View
- * @subpackage Helper
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_View
  */
 
-/**
- * @namespace
- */
 namespace Zend\View\Helper;
 
 use Zend\View\Exception;
+use Zend\View\Helper\AbstractHelper;
 
 /**
- * Helper for retrieving an asset URL.
+ * Helper for rendering asset urls, e.g. for assets located at a CDN.
  *
  * @package    Zend_View
  * @subpackage Helper
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class AssetUrl extends AbstractHelper
 {
-    /**
-     * Base URL for assets.
-     *
-     * @var string
-     */
-    protected $assetBaseUrl;
-    
-    /**
-     * Suffix for assets.
-     *
-     * @var string
-     */
-    protected $suffix;
 
     /**
-     * Returns the URL for an asset.
+     * A map of registered asset urls.
      *
-     * If a suffix was set, it will be appended as query parameter to the URL.
-     *
-     * @param  string  $file
-     * @parma  boolean $omitSuffix
-     * @return string
+     * @var array
      */
-    public function __invoke($file, $omitSuffix = false)
+    protected $assetUrls;
+
+    /**
+     * The default key.
+     *
+     * @var string
+     */
+    protected $defaultKey;
+
+    /**
+     * Constructs instance.
+     *
+     * @param  array $options
+     * @throws Exception\InvalidArgumentException
+     */
+    public function __construct($options = array())
     {
-        if (null === $this->assetBaseUrl) {
-            throw new Exception\RuntimeException('No asset base URL provided');
+        if (!is_array($options)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'options',
+                'an array'
+            ));
         }
 
-        $url = $this->assetBaseUrl . '/' . ltrim($file, '/');
-        
-        if (!$omitSuffix && null !== $this->suffix) {
-            if (strpos($url, '?') === false) {
-                $url .= '?' . $this->suffix;
+        if (isset($options['default_key'])) {
+            $this->setDefaultKey($options['default_key']);
+        }
+
+        if (isset($options['asset_urls'])) {
+            $this->setAssetUrls($options['asset_urls']);
+        }
+    }
+
+    /**
+     * Invokes the helper.
+     *
+     * @param  string $file
+     * @param  string $key
+     * @return string
+     * @throws Exception\InvalidArgumentException
+     * @throws Exception\BadMethodCallException
+     */
+    public function __invoke($file, $key = null)
+    {
+        if (!is_string($file)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'file',
+                'a string'
+            ));
+        }
+
+        if (null !== $key) {
+
+            if (!is_string($key)) {
+                throw new Exception\InvalidArgumentException(sprintf(
+                    'Parameter "%s" needs to be specified as %s.',
+                    'key',
+                    'a string'
+                ));
+            }
+
+            if (!isset($this->assetUrls[$key])) {
+                throw new Exception\BadMethodCallException(sprintf(
+                    'An asset url has not been registered for the key "%s".',
+                    $key
+                ));
+            }
+
+        }
+
+        if (null === $key) {
+            $key = $this->getDefaultKey();
+        }
+
+        if (null === $key) {
+            return $file;
+        }
+
+        $url = $file;
+
+        if ('' != $this->assetUrls[$key]['prefix']) {
+
+            $url = sprintf(
+                '%s/%s',
+                $this->assetUrls[$key]['prefix'],
+                ltrim(
+                    $url,
+                    '/'
+                )
+            );
+        }
+
+        if ('' != $this->assetUrls[$key]['suffix']) {
+            if (false === strpos($url, '?')) {
+                $url = sprintf(
+                    '%s?%s',
+                    $url,
+                    urlencode($this->assetUrls[$key]['suffix'])
+                );
             } else {
-                $url .= '&' . $this->suffix;
+                $url = sprintf(
+                    '%s&%s',
+                    $url,
+                    urlencode($this->assetUrls[$key]['suffix'])
+                );
             }
         }
 
@@ -79,26 +141,128 @@ class AssetUrl extends AbstractHelper
     }
 
     /**
-     * Set the asset base URL.
+     * Sets a bunch of asset urls.
      *
-     * @param  string $assetBaseUrl
-     * @return AssetUrl
+     * @param  array $assetUrls
+     * @throws Exception\InvalidArgumentException
      */
-    public function setAssetBaseUrl($assetBaseUrl)
+    public function setAssetUrls($assetUrls = array())
     {
-        $this->assetBaseUrl = rtrim($assetBaseUrl, '/');
-        return $this;
+        if (!is_array($assetUrls)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'assetUrls',
+                'an array'
+            ));
+        }
+
+        $this->defaultKey = null;
+        $this->assetUrls = array();
+
+        foreach ($assetUrls as $key => $assetUrlOptions) {
+            $this->setAssetUrl(
+                $key,
+                isset($assetUrlOptions['prefix']) ? $assetUrlOptions['prefix'] : '',
+                isset($assetUrlOptions['suffix']) ? $assetUrlOptions['suffix'] : ''
+            );
+        }
+
     }
 
     /**
-     * Set a suffix for assets.
+     * Sets an asset url.
      *
+     * @param  string $key
+     * @param  string $prefix
      * @param  string $suffix
-     * @return AssetUrl
+     * @throws Exception\InvalidArgumentException
      */
-    public function setSuffix($suffix)
+    public function setAssetUrl($key = null, $prefix = '', $suffix = '')
     {
-        $this->suffix = urlencode($suffix);
-        return $this;
+        if (!is_string($key)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'key',
+                'a string'
+            ));
+        }
+
+        if (!is_string($prefix)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'prefix',
+                'a string'
+            ));
+        }
+
+        if (!is_string($suffix)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'suffix',
+                'a string'
+            ));
+        }
+
+        $prefix = rtrim(
+            $prefix,
+            '/'
+        );
+
+        if ('' == $prefix) {
+            $prefix = null;
+        }
+
+        $suffix = ltrim(
+            $suffix,
+            '?'
+        );
+
+        if ('' == $suffix) {
+            $suffix = null;
+        }
+
+        $this->assetUrls[$key] = array(
+            'prefix' => $prefix,
+            'suffix' => $suffix,
+        );
     }
+
+    /**
+     * Returns default key.
+     *
+     * @return null|string
+     */
+    public function getDefaultKey()
+    {
+        if (null === $this->defaultKey
+            && is_array($this->assetUrls)
+            && 0 < count($this->assetUrls)) {
+
+            $keys = array_keys($this->assetUrls);
+            $this->defaultKey = reset($keys);
+
+        }
+
+        return $this->defaultKey;
+    }
+
+    /**
+     * Sets default key.
+     *
+     * @param  string $key
+     * @throws Exception\InvalidArgumentException
+     */
+    public function setDefaultKey($key)
+    {
+        if (!is_string($key)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s.',
+                'file',
+                'a string'
+            ));
+        }
+
+        $this->defaultKey = $key;
+    }
+
 }
